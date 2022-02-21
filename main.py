6afa4d400ad1fd895918a4ec92009b80de1aad0c
lewandowskicommits <vesilth@gmail.com>
2022-02-21 12:59:13 +0100

    add plot of rate change

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from datetime import date
+from datetime import date, timedelta
 import streamlit as st
 import requests
 import pandas as pd
+import plotly.express as px
 
 if __name__ == '__main__':
-    st.title('henlo into my app')
+    st.title('NBP currency converter')
 
+    minDate = '2002-01-02'
     maxDate = requests.get(f'https://api.nbp.pl/api/exchangerates/tables/A/').json()[0]['effectiveDate']
 
     rateDate = st.date_input('Select date on which the rate was valid',
-                             min_value=date.fromisoformat('2002-01-02'),
+                             min_value=date.fromisoformat(minDate),
                              max_value=date.fromisoformat(maxDate),
                              value=date.fromisoformat(maxDate))
 
@@ -24,12 +26,31 @@ if __name__ == '__main__':
         rate = round(df[df["currency"] == chosenRate].iloc[0]["mid"], 2)
         rateCode = df[df["currency"] == chosenRate].iloc[0]["code"]
 
-        st.header(f'1 PLN = {rate} {rateCode}')
+        st.write(f'1 PLN = {rate} {rateCode}')
         plnAmount = int(st.number_input('How much PLN to convert?', 0))
 
         st.header(f'{plnAmount} PLN = {round(rate*plnAmount, 2)} {rateCode}')
     except ValueError:
         st.error('The date you picked has no records')
 
+    rateHistoryTimeDelta = timedelta(days=367)
+    maxDateDatetime = date.fromisoformat(maxDate)
+    rateHistoryMinDate = maxDateDatetime-rateHistoryTimeDelta
+
+    request_string = f'http://api.nbp.pl/api/exchangerates/rates/A/{rateCode}/{rateHistoryMinDate}/{maxDate}?format=json'
+    r = requests.get(request_string).json()
+    rateHistoryDf = pd.json_normalize(r['rates'])
+    rateHistoryDf = rateHistoryDf.drop(['no'], axis = 1)
+    rateHistoryDf['effectiveDate'] = pd.to_datetime(rateHistoryDf['effectiveDate'],
+                                                    format="%Y/%m/%d")
+
+    rateHistoryFig = px.line(rateHistoryDf,
+                            x='effectiveDate',
+                            y = 'mid',
+                            title = f'Rate of PLN to {rateCode} in last 367 days')
+
+    st.plotly_chart(rateHistoryFig)
+
 else:
     pass
+
